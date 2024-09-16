09773d9ca55b99b6394bdeb0a01ba749fd1cc487
Michael Bangas <michael.bangas@vector.com>
2024-09-16 17:55:31 +0200

    Introduce SVG rasterizer logic
    Feature Proposal: Rasterization of SVGs at Runtime for Eclipse Icons
    Fixes https://github.com/eclipse-platform/eclipse.platform.swt/issues/1438
    
    Eclipse currently loads icons exclusively as raster graphics (e.g., `.png`), without support for vector formats like `.svg`. A major drawback of raster graphics is their inability to scale without degrading image quality. Additionally, generating icons of different sizes requires manually rasterizing SVGs outside Eclipse, leading to unnecessary effort and many icon files.
    
    This PR introduces support for vector graphics in Eclipse, enabling SVGs to be used for icons. Existing PNG icons will continue to be loaded alongside SVGs, allowing the use of the new functionality without the need to replace all PNG files at once.
    
    ---
    - **How It Works**:
      - To use SVG icons, simply place the SVG file in the bundle and reference it in the `plugin.xml` and other necessary locations, as is done for PNGs. No additional configuration is required.
      - At runtime, Eclipse uses the library JSVG to rasterize the SVG into a raster image of the desired size, eliminating the need for scaling. My analysis shows that JSVG is the most suitable Java library for this purpose.
      - You need to write the flag `-Dswt.autoScale=quarter` into your `eclipse.ini` file or into the run arguments of a new configuration.

---
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/graphics/SVGRasterizerRegistry.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Vector Informatik GmbH and others.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License 2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors: Michael Bangas (Vector Informatik GmbH) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.graphics;
+
+/**
+ * A registry for managing the instance of an {@link SVGRasterizer} implementation.
+ * This allows for the registration and retrieval of a single rasterizer instance.
+ *
+ * @since 3.129
+ */
+public class SVGRasterizerRegistry {
+
+	/**
+     * The instance of the registered {@link SVGRasterizer}.
+     */
+	private static SVGRasterizer rasterizer;
+
+	 /**
+     * Registers the provided implementation of {@link SVGRasterizer}.
+     * If a rasterizer has already been registered, subsequent calls to this method
+     * will have no effect.
+     *
+     * @param implementation the {@link SVGRasterizer} implementation to register.
+     */
+	public static void register(SVGRasterizer implementation) {
+		if (rasterizer == null) {
+			rasterizer = implementation;
+		}
+	}
+
+	/**
+     * Retrieves the currently registered {@link SVGRasterizer} implementation.
+     *
+     * @return the registered {@link SVGRasterizer}, or {@code null} if no implementation
+     *         has been registered.
+     */
+	public static SVGRasterizer getRasterizer() {
+		return rasterizer;
+	}
+}
